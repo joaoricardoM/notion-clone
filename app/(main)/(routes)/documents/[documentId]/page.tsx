460c814473fd57d8d664bf02cbbe8a3aa400bb8a
joaoricardoM <joao_pia_mds@hotmail.com>
2023-11-17 12:46:27 -0300

    feat: create documentId

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import React, { useMemo } from "react"
+
+import { useQuery } from "convex/react"
+
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import dynamic from "next/dynamic"
+import { Toolbar } from "@/components/toolbar"
+
+interface DocumentIdPage {
+  params: {
+    documentId: Id<"documents">
+  }
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPage) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  })
+
+  // const Editor = useMemo(() => dynamic(() => import("@/components/editor"), {ssr: false}), [])
+
+  if (document === undefined) {
+    return <div>loading... </div>
+  }
+
+  if (document === null) {
+    return <div>Not Found</div>
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  )
+}
+
+export default DocumentIdPage
