6b10ea905091495686c0404a3b7eeaac622cecf9
joaoricardoM <joao_pia_mds@hotmail.com>
2023-11-23 16:52:55 -0300

    feat: create editor component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -2,13 +2,14 @@
 
 import React, { useMemo } from "react"
 
-import { useQuery } from "convex/react"
+import { useMutation, useQuery } from "convex/react"
 
 import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import dynamic from "next/dynamic"
 import { Toolbar } from "@/components/toolbar"
 import { Cover } from "@/components/cover"
+import { Skeleton } from "@/components/ui/skeleton"
 
 interface DocumentIdPage {
   params: {
@@ -17,14 +18,38 @@ interface DocumentIdPage {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPage) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    []
+  )
+
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId
   })
 
-  // const Editor = useMemo(() => dynamic(() => import("@/components/editor"), {ssr: false}), [])
+  const update = useMutation(api.documents.update)
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content
+    })
+  }
 
   if (document === undefined) {
-    return <div>loading... </div>
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    )
   }
 
   if (document === null) {
@@ -36,6 +61,7 @@ const DocumentIdPage = ({ params }: DocumentIdPage) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   )
